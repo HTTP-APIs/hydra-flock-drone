8c3dfb3b077b892320619b7f39b5e75a76e1d883
Akshay Dahiya <xadahiya@gmail.com>
2017-07-28 00:15:12 +0530

    add distance related algos

---
+++ flock_drone/mechanics/distance.py
@@ -0,0 +1,82 @@
+from haversine import haversine
+import math
+
+
+
+# Distances are measured in miles/Kilometers.
+# Longitudes and latitudes are measured in degrees.
+# Earth is assumed to be perfectly spherical.
+
+# earth_radius = 3960.0 ## For miles
+earth_radius = 6371.0 ## For Kilometers
+degrees_to_radians = math.pi/180.0
+radians_to_degrees = 180.0/math.pi
+
+def change_in_latitude(distance):
+    """Given a distance north, return the change in latitude."""
+    return (distance/earth_radius)*radians_to_degrees
+
+def change_in_longitude(latitude, distance):
+    """Given a latitude and a distance west, return the change in longitude."""
+    # Find the radius of a circle around the earth at given latitude.
+    r = earth_radius*math.cos(latitude*degrees_to_radians)
+    return (distance/r)*radians_to_degrees
+
+
+# print(change_in_latitude(-1))
+# print(change_in_longitude(a[0], -1))
+
+def convert_direction_to_north_or_west(distance_moved, direction):
+    """Convert East and South direction to North and East."""
+    if direction in ["S","E"]:
+        if direction == "S":
+            distance_moved = distance_moved * -1
+            direction = "N"
+        elif direction == "E":
+            distance_moved = distance_moved * -1
+            direction = "W"
+
+    return distance_moved, direction
+
+def gen_new_coordinates_from_change_in_coordinates(old_coordinates, change_in_coordinates):
+    """Calculate new coordinates given coordinates(lat,lon) and change_in_coordinates(lat,lon)."""
+    return tuple(map(lambda x, y: x - y, old_coordinates, change_in_coordinates))
+
+
+def get_new_coordinates(old_coordinates, distance_moved, direction):
+    """ Get new coordinates given old coordinates (lat,lon), distance moved in kilometers
+    direction of movement [N, S, E, W]."""
+
+    ## Convert directions if needed
+    distance_moved, direction = convert_direction_to_north_or_west(distance_moved, direction)
+
+    if direction == "N":
+        latitude_change = change_in_latitude(distance_moved)
+        change_in_coordinates = (latitude_change, 0)
+
+    elif direction == "W":
+        latitude = old_coordinates[0]
+        longitude_change = change_in_longitude(latitude, distance_moved)
+        change_in_coordinates = (0, longitude_change)
+    else:
+        raise "Not a valid direction of movement! Please use one of  ['N', 'S', 'E', 'W']"
+
+    return gen_new_coordinates_from_change_in_coordinates(old_coordinates, change_in_coordinates)
+
+
+
+
+def get_distance_between_coordinates(a,b):
+    """Get the distance between two sets of coordinates."""
+    return haversine(a,b)
+
+
+
+if __name__ == "__main__":
+
+    a = (-30.040397656836609, -30.03373871559225) ## (lat,lon)
+    print("Initial Coordinates", a)
+    b = get_new_coordinates(a, 500, "S")
+    print("Final Coordinates", b)
+    distance_between_coordinates = get_distance_between_coordinates(a,b)
+    print("Distance_between_coordinates", distance_between_coordinates)
