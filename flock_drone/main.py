4d0ada0d0ce3a957769352ae0d74e4bb02ee4b1c
Akshay Dahiya <xadahiya@gmail.com>
2017-08-15 16:00:09 +0530

    updated to use latest version of hydrus

--- flock_drone/main.py
+++ flock_drone/main.py
@@ -7,8 +7,8 @@ parentDir = os.path.abspath(os.path.join(curDir, os.pardir))
 sys.path.insert(0, parentDir)
 
 from hydrus.app import app_factory
-# from hydrus.utils import set_session, set_doc, set_hydrus_server_url
-from hydrus.app import set_session, set_doc, set_hydrus_server_url
+from hydrus.utils import set_session, set_doc, set_hydrus_server_url
+# from hydrus.app import set_session, set_doc, set_hydrus_server_url
 from hydrus.data import doc_parse
 from hydrus.hydraspec import doc_maker
 from sqlalchemy import create_engine
